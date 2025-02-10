49ad25a8140b85206899e23b84b4bb70e4051e45
vantutran2k1 <vantu.tran2k1@gmail.com>
2025-02-10 21:20:58 +0700

    Initial commit

---
+++ src/test/java/com/vantutran2k1/todoapp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantutran2k1.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
